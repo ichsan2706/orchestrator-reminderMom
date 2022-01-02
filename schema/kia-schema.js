e4191b5ec1da15f37548993827bf117cb07e6e2d
ichsan2706 <ichsanrosaldi27@gmail.com>
2022-01-02 23:07:58 +0700

    detail tips

--- schema/kia-schema.js
+++ schema/kia-schema.js
@@ -18,12 +18,23 @@ const typeDefs = gql`
         id_parent:String
     }
 
+    type Tips {
+        title: String,
+        description: String,
+        nutrition: String,
+        stimulation: String,
+        tips: String,
+        phase: String
+    }
+
     type Message {
         message:String
         status:Int
     }
 
     type Query {
+        getTips: [Tips]
+        detailTips(phase: String): Tips
         getChildrens(id_parent:String): [Children]
     }
 
@@ -38,6 +49,22 @@ const resolvers = {
             try {
                 let { data: childrens } = await axios.get('http://localhost:4002/children/'+args.id_parent)
                 return childrens.result
+            } catch (error) {
+                return error
+            }
+        },
+        getTips: async () => {
+            try {
+                let {data: tips} = await axios.get(`http://localhost:4002/tips`)
+                return tips
+            } catch (error) {
+                return error
+            }
+        },
+        detailTips: async (_, args) => {
+            try {
+                let {data: tip} = await axios.get(`http://localhost:4002/tips/${args.phase}`)
+                return tip
             } catch (error) {
                 return error
             }
