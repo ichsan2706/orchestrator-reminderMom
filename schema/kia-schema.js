2983225a8fd7f8dfa4347081d221f9dc96f40ed1
= <=>
2022-01-03 13:28:22 +0700

    add treatment and medical record at kia schema

--- schema/kia-schema.js
+++ schema/kia-schema.js
@@ -27,6 +27,26 @@ const typeDefs = gql`
         phase: String
     }
 
+    type MedicalRecord {
+        id_children: Int,
+        id_treatment: Int,
+        id_midwife: Int,
+        place:String,
+        imgUrl:String,
+        height: String,
+        weight: String,
+        headCirc: String,
+        note: String,
+        createdAt: String
+    }
+
+    type Treatment{
+        id:Int,
+        name: String,
+        description: String,
+        month: Int
+    }
+
     type Message {
         message:String
         status:Int
@@ -36,6 +56,9 @@ const typeDefs = gql`
         getTips: [Tips]
         detailTips(phase: String): Tips
         getChildrens(id_parent:String): [Children]
+        getTreathment(age: Int):[Treatment]
+        getMedicalRecordPerChild(id_children: Int):[MedicalRecord]
+        getTreatments:[Treatment]
     }
 
     type Mutation {
@@ -64,6 +87,30 @@ const resolvers = {
             try {
                 let {data: tip} = await axios.get(`http://localhost:4002/tips/${args.phase}`)
                 return tip
+            } catch (error) {
+                return error
+            }
+        },
+        getTreathment: async (_, args) => {
+            try {
+                let { data: treathment } = await axios.get(`http://localhost:4002/treatment/${args.age}`)
+                return treathment
+            } catch (error) {
+                return error
+            }
+        },
+        getTreatments: async () => {
+            try {
+                let { data: treatments } = await axios.get(`http://localhost:4002/treatment`)
+                return treatments.result
+            } catch (error) {
+                return error
+            }
+        },
+        getMedicalRecordPerChild: async (_, args) => {
+            try {
+                let { data: medicalRecord } = await axios.get(`http://localhost:4002/medicalRecord/${args.id_children}`)
+                return medicalRecord.result
             } catch (error) {
                 return error
             }
